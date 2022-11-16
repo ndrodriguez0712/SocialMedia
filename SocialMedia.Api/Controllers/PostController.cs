a90c2cb624304f7d75f41c585f8b5b68d2b0d1a4
Nicolás D. Rodriguez <nico.d.rodriguez@hotmail.com>
2022-11-16 14:10:21 -0300

    Part 6 - DTO and Automapper.

--- SocialMedia.Api/Controllers/PostController.cs
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using SocialMedia.Core.DTOs;
+using SocialMedia.Core.Entities;
 using SocialMedia.Core.Interfaces;
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SocialMedia.Api.Controllers
@@ -9,23 +15,36 @@ namespace SocialMedia.Api.Controllers
     public class PostController : ControllerBase
     {
         private readonly IPostRepository _postRepository;
-        public PostController(IPostRepository postRepository)
+        private readonly IMapper _mapper;
+        public PostController(IPostRepository postRepository, IMapper mapper)
         {
             _postRepository = postRepository;
+            _mapper = mapper;
         }
 
         [HttpGet]
         public async  Task<IActionResult> GetPosts()
         {
             var posts = await _postRepository.GetPosts();
+            var postsDto = _mapper.Map<IEnumerable<PostDto>>(posts);
 
-            return Ok(posts);
+            return Ok(postsDto);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetPost(int id)
         {
             var post = await _postRepository.GetPost(id);
+            var postDto = _mapper.Map<PostDto>(post);
+
+            return Ok(postDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(PostDto postDto)
+        {
+            var post = _mapper.Map<Post>(postDto);
+            await _postRepository.InsertPost(post);
 
             return Ok(post);
         }
