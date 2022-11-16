7d53c60b9e665c96ed23c56625c7f5688bfab85c
Nicolás D. Rodriguez <nico.d.rodriguez@hotmail.com>
2022-11-16 13:00:09 -0300

    Add project files.

---
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.Interfaces;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostRepository _postRepository;
+        public PostController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        [HttpGet]
+        public async  Task<IActionResult> GetPosts()
+        {
+            var posts = await _postRepository.GetPosts();
+
+            return Ok(posts);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPost(int id)
+        {
+            var post = await _postRepository.GetPost(id);
+
+            return Ok(post);
+        }
+    }
+}
---
+++ SocialMedia.Infrastructure/Data/SocialMediaContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using SocialMedia.Core.Entities;
+using SocialMedia.Infrastructure.Data.Configurations;
+
+namespace SocialMedia.Infrastructure.Data
+{
+    public partial class SocialMediaContext : DbContext
+    {
+        public SocialMediaContext()
+        {
+        }
+
+        public SocialMediaContext(DbContextOptions<SocialMediaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Comment> Comments { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CommentConfiguration());
+            modelBuilder.ApplyConfiguration(new PostConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+    }
+}
